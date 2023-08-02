a41fb466cce02e6a5873102c25920274194160bc
Renate Winther Ravnaas <renate.ravnaas@amedia.no>
2023-08-02 23:32:34 +0200

    fix: just add a comment to import adapter-netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+//import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
