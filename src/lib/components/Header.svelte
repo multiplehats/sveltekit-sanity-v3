4775c06c360ac690ae2bcc6c97b734672240f032
Renate Winther Ravnaas <renate.ravnaas@amedia.no>
2023-09-22 22:30:09 +0200

    fix: move insta link

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -10,8 +10,58 @@
         </a>
       </div>
 
-      
+      <p class="shrink-0">
+        <a
+          href="https://www.instagram.com/goatnbike/"
+          title=""
+          role="button"
+          class="inline-flex items-center justify-center px-4 py-2 text-sm font-bold text-brand transition-all duration-200 border border-transparent hover:bg-transparent focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand"
+        >
+        <i class="gg-instagram"></i>
+        </a>
+    </p>
     </div>
   </div>
 </div>
 
+<style>
+.gg-instagram {
+  box-sizing: border-box;
+  position: relative;
+  display: block;
+  transform: scale(var(--ggs,1));
+  border: 2px solid transparent;
+  box-shadow: 0 0 0 2px;
+  border-radius: 4px;
+  width: 20px;
+  height: 20px
+}
+.gg-instagram::after,
+.gg-instagram::before {
+  content: "";
+  display: block;
+  box-sizing: border-box;
+  position: absolute
+}
+.gg-instagram::after {
+  border: 2px solid;
+  left: 3px;
+  width: 10px;
+  height: 10px;
+  border-radius: 100%;
+  top: 3px
+}
+.gg-instagram::before {
+  border-radius: 3px;
+  width: 2px;
+  height: 2px;
+  background: currentColor;
+  right: 1px;
+  top: 1px
+}
+h1,h2, text-base {
+font-family: 'Inclusive Sans', sans-serif;
+}
+
+
+</style>
